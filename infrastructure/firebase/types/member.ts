72d750381670bf87ac2c5cb8ba765c94cdf9902d
satorudev976 <satorudev976@gmail.com>
2026-01-18 08:46:46 +0900

    delete member type

--- infrastructure/firebase/types/member.ts
+++
@@ -1,8 +0,0 @@
-import { Timestamp } from 'firebase/firestore';
-
-export interface Member {
-  userId: string
-  role: 'owner' | 'family'
-  nickname: string
-  joinedAt: Timestamp
-}
--- infrastructure/firebase/types/user.ts
+++ infrastructure/firebase/types/user.ts
@@ -3,6 +3,7 @@ import { Timestamp } from 'firebase/firestore';
 export interface User {
   userId: string 
   familyId: string
+  role: 'owner' | 'family'
   nickname: string | null
   createdAt: Timestamp
 }
