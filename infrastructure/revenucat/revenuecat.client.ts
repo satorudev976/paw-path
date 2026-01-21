c819cf9c0212e8073a2c9a9c2037243efb6e83c8
satorudev976 <satorudev976@gmail.com>
2026-01-22 05:02:54 +0900

    modify revenucat

--- infrastructure/revenucat/revenuecat.client.ts
+++ infrastructure/revenucat/revenuecat.client.ts
@@ -1,9 +1,8 @@
 import Purchases from 'react-native-purchases'
 import Constants from 'expo-constants'
 
-export const initRevenueCat = (userId: string) => {
+export const initRevenueCat = () => {
   Purchases.configure({
     apiKey: Constants.expoConfig?.extra?.revenuCat?.iosKey,
-    appUserID: userId,
   })
 }
--- services/revenuecat.init.ts
+++
@@ -1,9 +0,0 @@
-import Purchases from 'react-native-purchases'
-import Constants from 'expo-constants'
-
-export const initRevenueCat = async (userId: string) => {
-  await Purchases.configure({
-    apiKey: Constants.expoConfig?.extra?.revenueCatApiKey,
-    appUserID: userId,
-  })
-}
