86c179187e7222e5e403fb560fbe62c0bb8cefa9
satorudev976 <satorudev976@gmail.com>
2026-01-22 07:38:58 +0900

    add log

--- infrastructure/revenucat/revenuecat.client.ts
+++ infrastructure/revenucat/revenuecat.client.ts
@@ -2,6 +2,7 @@ import Purchases from 'react-native-purchases'
 import Constants from 'expo-constants'
 
 export const initRevenueCat = () => {
+  console.log('RevenuCat初期化')
   Purchases.configure({
     apiKey: Constants.expoConfig?.extra?.revenuCat?.iosKey,
   })
