8de91fa958923f3fcda3c3fc7ae644ebbfea6597
satorudev976 <satorudev976@gmail.com>
2026-01-22 04:06:10 +0900

    fix index

--- app/index.tsx
+++ app/index.tsx
@@ -6,18 +6,20 @@ import { Redirect } from 'expo-router';
 export default function Index() {
   const { firebaseUser, isLoading: authLoading } = useAuth();
   const { user, isLoading: userLoading } = useUser();
-  console.log("Index", firebaseUser);
-  console.log("Index", user);
 
   if (authLoading) {
-    return null // or Splash
+    return null; // or Splash
   }
 
   if (!firebaseUser) {
     return <Redirect href="/(auth)/login" />;
   }
 
-  if (!user) {
+  if (userLoading) {
+    return null;
+  }
+
+  if (!user?.nickname) {
     return <Redirect href="/(onboarding)/nickname" />;
   }
 
