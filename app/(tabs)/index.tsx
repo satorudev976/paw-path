e3bcd396680989d8fbe03b5d8ffaf5e2bfbbb726
satorudev976 <satorudev976@gmail.com>
2026-01-24 16:34:41 +0900

    fix warn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { useWalkRecording } from '@/hooks/use-walk-recording';
 import { StyleSheet, View } from 'react-native';
-import { IdleState } from '../(home)/idel-state';
-import { RecordingState } from '../(home)/recording-state';
+import IdleState from '../(home)/idel-state';
+import RecordingState from '../(home)/recording-state';
 import Toast from '@/components/ui/toast';
 import { useToast } from '@/hooks/toast';
 
